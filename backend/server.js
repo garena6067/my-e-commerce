3622290f785e452085203e7001a13f8ffd8318c3
User <User@DESKTOP-FAAFAA4>
2025-09-06 16:41:52 +0600

    Created a project, routes, controller.

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+//const express = require("express");
+
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+app.use("/api/auth", authRoutes)
+
+app.listen(PORT, () =>{
+    console.log("Server running on http://localhost:" + PORT);
+});
